0312b6c52bd63636fc6133c4ba1febca6fbb312b
Nick Dawson <neonkoala@gmail.com>
2011-02-28 00:31:32 +0000

    Optimisations. I hope.

--- main.m
+++ main.m
@@ -1,42 +1,27 @@
 #import <UIKit/UIKit.h>
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-// Determines if the device is capable of running on this platform. If your toggle is device specific like only for
-// 3g you would check that here.
 BOOL isCapable()
 {
-	if(![[NSFileManager defaultManager] fileExistsAtPath:@"/openiboot"]) {
+	if(![[NSFileManager defaultManager] fileExistsAtPath:@"/openiboot"]) {		//Check if OpeniBoot is installed
 		return NO;
 	}
 	
 	return YES;
 }
 
-// This runs when iPhone springboard resets. This is on boot or respring.
 BOOL isEnabled()
 {
 	return NO;
 }
 
-// This function is optional and should only be used if it is likely for the toggle to become out of sync
-// with the state while the iPhone is running. It must be very fast or you will slow down the animated
-// showing of the sbsettings window. Imagine 12 slow toggles trying to refresh tate on show.
-BOOL getStateFast()
-{
-	return isEnabled();
-}
-
-// Pass in state to set. YES for enable, NO to disable.
 void setState(BOOL Enable)
 {
-	notify_post("be.dawson.quickboot");
+	if(Enable) {
+		notify_post("be.dawson.quickboot");		//Call the actual function as we need to be root to modify IOReg
+	}
 }
 
-// Amount of time spinner should spin in seconds after the toggle is selected.
 float getDelayTime()
 {
-	return 10.0f;
+	return 10.0f;	//10 seconds until reboot kicks in
 }
