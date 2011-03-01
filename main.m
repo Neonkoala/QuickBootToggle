cdb53d4effd145c8c617fcdb3cad2cb535eeb2f8
Nick Dawson <neonkoala@gmail.com>
2011-03-01 20:42:25 +0000

    Now closes the window first before reboot for shits & giggles.

--- main.m
+++ main.m
@@ -1,7 +1,27 @@
 #import <UIKit/UIKit.h>
 
-BOOL isCapable()
-{
+UIWindow* getAppWindow() {
+	UIWindow* TheWindow = nil;
+	UIApplication* App = [UIApplication sharedApplication];
+	NSArray* windows = [App windows];
+	int i;
+	
+	for(i = 0; i < [windows count]; i++) {
+		TheWindow = [windows objectAtIndex:i];
+		if([TheWindow respondsToSelector:@selector(getCurrentTheme)])
+		{
+			break;
+		}
+	}
+	
+	if(i == [windows count]) {
+		TheWindow = [App keyWindow];
+	}
+	
+	return TheWindow;
+}
+
+BOOL isCapable() {
 	if(![[NSFileManager defaultManager] fileExistsAtPath:@"/openiboot"]) {		//Check if OpeniBoot is installed
 		return NO;
 	}
@@ -9,19 +29,21 @@ BOOL isCapable()
 	return YES;
 }
 
-BOOL isEnabled()
-{
+BOOL isEnabled() {
 	return NO;
 }
 
-void setState(BOOL Enable)
-{
+void setState(BOOL Enable) {
 	if(Enable) {
 		notify_post("be.dawson.quickboot");		//Call the actual function as we need to be root to modify IOReg
+		
+		sleep(3);
+		
+		UIWindow* Window = getAppWindow();
+		[Window closeButtonPressed];
 	}
 }
 
-float getDelayTime()
-{
-	return 10.0f;	//10 seconds until reboot kicks in
+float getDelayTime() {
+	return 3.0f;	//10 seconds until reboot kicks in
 }
